8e06ef28a8d8bb647f91593fd456b3cfd6a37c93
Aitor <aitor.velasco.lora@gmail.com>
2022-09-22 18:08:25 +0200

    Componente de prueba

---
+++ components/MiPrimerComponente.jsx
@@ -0,0 +1,10 @@
+class MiPrimerComponente extends Component {
+    render() {
+      return (
+        <div>
+          <h1>Hola Antonio</h1>
+          <h1>Hola Pepe</h1>
+        </div>
+      );
+    }
+  }
